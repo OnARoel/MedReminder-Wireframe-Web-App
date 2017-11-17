1798da7f2dbbe9416c6b802be4cfac3470da35ac
mathieuvu-noreau <mvunorea@mail.uoguelph.ca>
2017-11-17 17:22:52 -0500

    removed node project

--- signup-node/templates/js/login.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('#show-pass').change(function () {
-        if ($(this).is(':checked')) {
-            $("#password-field").attr('type', '');
-        } else {
-            $("#password-field").attr('type', 'password');
-        }
-    });
-});
