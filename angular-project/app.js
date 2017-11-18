175214381e9174101833bd7073e4bf45aaf9d717
mathieuvu-noreau <mvunorea@mail.uoguelph.ca>
2017-11-17 22:58:47 -0500

    removed rewgidster page, it is now in a modal

--- angular-project/app.js
+++ angular-project/app.js
@@ -7,6 +7,7 @@
         .run(run);
 
     config.$inject = ['$routeProvider', '$locationProvider'];
+
     function config($routeProvider, $locationProvider) {
         $routeProvider
             .when('/', {
@@ -20,17 +21,13 @@
                 templateUrl: 'login/login.view.html',
                 controllerAs: 'vm'
             })
-
-            .when('/register', {
-                controller: 'RegisterController',
-                templateUrl: 'register/register.view.html',
-                controllerAs: 'vm'
-            })
-
-            .otherwise({ redirectTo: '/login' });
+            .otherwise({
+                redirectTo: '/login'
+            });
     }
 
     run.$inject = ['$rootScope', '$location', '$cookies', '$http'];
+
     function run($rootScope, $location, $cookies, $http) {
         // keep user logged in after page refresh
         $rootScope.globals = $cookies.getObject('globals') || {};
@@ -40,7 +37,7 @@
 
         $rootScope.$on('$locationChangeStart', function (event, next, current) {
             // redirect to login page if not logged in and trying to access a restricted page
-            var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
+            var restrictedPage = $.inArray($location.path(), ['/login']) === -1;
             var loggedIn = $rootScope.globals.currentUser;
             if (restrictedPage && !loggedIn) {
                 $location.path('/login');
--- angular-project/login/login.controller.js
+++ angular-project/login/login.controller.js
@@ -5,11 +5,13 @@
         .module('app')
         .controller('LoginController', LoginController);
 
-    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
-    function LoginController($location, AuthenticationService, FlashService) {
+    LoginController.$inject = ['UserService', '$location', 'AuthenticationService', '$rootScope', 'FlashService'];
+
+    function LoginController(UserService, $location, AuthenticationService, $rootScope, FlashService) {
         var vm = this;
 
         vm.login = login;
+        vm.register = register;
 
         (function initController() {
             // reset login status
@@ -27,6 +29,23 @@
                     vm.dataLoading = false;
                 }
             });
+        };
+
+        function register() {
+            vm.dataLoading = true;
+            UserService.Create(vm.user)
+                .then(function (response) {
+                    if (response.success) {
+                        FlashService.Success('Registration successful', true);
+                        vm.dataLoading = false;
+                        $(function () {
+                            $('#modal-signup').hide();
+                        });
+                    } else {
+                        FlashService.Error(response.message);
+                        vm.dataLoading = false;
+                    }
+                });
         };
     }
 
